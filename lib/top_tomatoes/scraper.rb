e29e7f6d08e54099d82a5b1e696f79618946c1e0
Caroline Berg <carolineeberg@gmail.com>
2018-11-13 22:18:41 +0000

    Film Scraper Method

--- lib/top_tomatoes/scraper.rb
+++ lib/top_tomatoes/scraper.rb
@@ -2,21 +2,28 @@ require 'pry'
 
 class TopTomatoes::Scraper 
   
-  website = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
-  top_box_office_films = website.css("#Top-Box-Office")
-  movie_collection = top_box_office_films.css("tr.sidebarInTheaterOpening")
+  def scrape_top_film_list
+     website = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+     top_box_office_films = website.css("#Top-Box-Office")
+     movie_collection = top_box_office_films.css("tr.sidebarInTheaterOpening")
  
-  movie_hash = {
-    :title => movie_collection.css("td.middle_col").collect {|top_film| top_film.text.strip}, 
-    :review_rating => movie_collection.css("span.tMeterScore").collect {|rating| rating.text}
-  }
+     film_hash = {
+       :title => movie_collection.css("td.middle_col").collect {|top_film| top_film.text.strip}, 
+       :review_rating => movie_collection.css("span.tMeterScore").collect {|rating| rating.text}
+      }
   
-  binding.pry 
-   
- 
+     film_hash
+  end
 
-
-
+  def self.scrape_film 
+  top_films_website = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+    base_path = 'https://www.rottentomatoes.com'
+    film_url = top_films_website.css(".movie_list#Top-Box-Office a").attr("href").value 
+    url = (base_path + "#{film_url}")
+    film_website = Nokogiri::HTML(open("#{url}"))
+    
+    binding.pry
+  end
  
   
   
