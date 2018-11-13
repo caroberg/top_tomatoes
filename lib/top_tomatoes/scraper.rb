820fc3c5015ac414a0ff788c4bc0b36e5ce90b79
Caroline Berg <carolineeberg@gmail.com>
2018-11-13 21:11:49 +0000

    review_rating scraper

--- lib/top_tomatoes/scraper.rb
+++ lib/top_tomatoes/scraper.rb
@@ -5,13 +5,10 @@ class TopTomatoes::Scraper
   website = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
   top_box_office_films = website.css("#Top-Box-Office")
   movie_collection = top_box_office_films.css("tr.sidebarInTheaterOpening")
-  
-    
-    
  
   movie_hash = {
-    :title => movie_collection.css("td.middle_col").collect {|top_film| top_film.text.strip} 
-    
+    :title => movie_collection.css("td.middle_col").collect {|top_film| top_film.text.strip}, 
+    :review_rating => movie_collection.css("span.tMeterScore").collect {|rating| rating.text}
   }
   
   binding.pry 
