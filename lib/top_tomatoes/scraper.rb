0f5acc5e38e6babcd3eef74d139fbbcbb60ca4c0
Caroline Berg <carolineeberg@gmail.com>
2018-11-20 14:41:38 +0000

    Scrape Top Films Method

--- lib/top_tomatoes/scraper.rb
+++ lib/top_tomatoes/scraper.rb
@@ -5,10 +5,12 @@ class TopTomatoes::Scraper
     def self.scrape_top_films 
       scrape_results = []
       page = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
-      film_table = page.css("div#homepage-top-box-office.listings") 
-      film_rows = film_table.css("tr.sidebarInTheaterOpening").each do |row| 
-        scrape_results << row
+      page.css("div#homepage-top-box-office.listings").each do |info| 
+        info.css("tr").each do |row| 
+          scrape_results << row
+        end
       end
+    
       i=1 
       while i = scrape_results.count do 
         url_name = scrape_results[i].css("a").attr("href").value
@@ -16,13 +18,10 @@ class TopTomatoes::Scraper
         title = scrape_results[i].css("td.middle_col").text.gsub("\n", "").strip
         review_rating = scrape_results[i].css(".tMeterScore").text
         box_office_revenue = scrape_results[i].css("td.right_col").text.gsub("\n", "").strip
-        instance = TopTomatoes::Film.new(title, review_rating, box_office_revenue, film_url)
+        #instance = TopTomatoes::Film.new(title, review_rating, box_office_revenue, film_url)
         i+=1 
+        
       end
-      
-      
-         binding.pry
-      
      
     end
     
