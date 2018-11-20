110257e67f9991eeb1babf6458a7650a47eb0bbb
Caroline Berg <carolineeberg@gmail.com>
2018-11-20 01:58:10 +0000

    Functional Scraper Method

--- lib/top_tomatoes/film.rb
+++ lib/top_tomatoes/film.rb
@@ -1,50 +1,7 @@
 require 'pry'
 
 class TopTomatoes::Film 
-  attr_accessor :title, :percentage, :box_office_revenue, :url, :genre, :director, :film_rating, :release_date, :film_summary, :film_url, :doc
   
-  @@all = []
-  
-  def initialize(title=nil, percentage=nil, box_office_revenue=nil, url=nil) 
-    @title = title 
-    @percentage = percentage 
-    @box_office_revenue = box_office_revenue 
-    @url = url 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def self.find(id) 
-    self.all[id-1] 
-  end 
-  
-  def film_summary 
-    @film_summary ||=
-  end
-  
-  def genre 
-    @genre ||=
-  end 
-  
-  def director 
-    @director ||=
-  end 
-  
-  def film_rating
-    @film_rating ||=
-  end 
-  
-  def release_date 
-    @release_date ||=
-  end
-  
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
-
   
 end
     
--- lib/top_tomatoes/scraper.rb
+++ lib/top_tomatoes/scraper.rb
@@ -2,33 +2,30 @@ require 'pry'
 
 class TopTomatoes::Scraper 
   
-    def get_page 
-      Nokogiri::HTML(open("https://www.rottentomatoes.com"))
-    end 
-    
-    def scrape_films_index 
-      self.get_page.css("div#homepage-top-box-office.listings")
+    def self.scrape_top_films 
+      scrape_results = []
+      page = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
+      film_table = page.css("div#homepage-top-box-office.listings") 
+      film_rows = film_table.css("tr.sidebarInTheaterOpening").each do |row| 
+        scrape_results << row
+      end
+      i=1 
+      while i = scrape_results.count do 
+        url_name = scrape_results[i].css("a").attr("href").value
+        film_url = "https://www.rottentomatoes.com" + url_name
+        title = scrape_results[i].css("td.middle_col").text.gsub("\n", "").strip
+        review_rating = scrape_results[i].css(".tMeterScore").text
+        box_office_revenue = scrape_results[i].css("td.right_col").text.gsub("\n", "").strip
+        instance = TopTomatoes::Film.new(title, review_rating, box_office_revenue, film_url)
+        i+=1 
+      end
+      
+      
+         binding.pry
+      
+     
     end
     
-    def scrape_films 
-      self.scrape_films_index.css("tr.sidebarInTheaterOpening")
-    end 
-    
-    def make_films
-      scrape_films.each do |film| 
-        TopTomatoes::Film.new_from_index_page(film)
-      end 
-    end
-    
-   # def scrape_films 
-    #  scrape_results = []
-     # films = self.scrape_films_index.css("tr.sidebarInTheaterOpening").collect {|row| row.text.strip} 
-      #  films.each do |film| 
-       #   scrape_results << self.scrape_films_index.text.strip
-      #end
-   # end
-    
-
-  end
+end
   
   
